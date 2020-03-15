0364c971f6bf3e45627fe0d9fea0190b525d9d91
wufan595112592 <595112592@qq.com>
2020-03-15 14:36:25 +0800

    配置路由组件规则

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,59 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/home'
 
 Vue.use(VueRouter)
-
+const Layout = () => import('./views/layout')
+const home = () => import('./views/home')
+const question = () => import('./views/question')
+const video = () => import('./views/video')
+const user = () => import('./views/user')
+const chat = () => import('./views/user/chat')
+const login = () => import('./views/login')
+const profile = () => import('./views/user/profile')
+const search = () => import('./views/search')
+const searchresult = () => import('./views/search/result')
+const article = () => import('./views/article')
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    component: Layout,
+    children: [{
+      path: '',
+      component: home
+    }, {
+      path: '/question',
+      component: question
+    }, {
+      path: '/video',
+      component: video
+    }, {
+      path: '/user',
+      component: user
+    }]
+  },
+  {
+    path: '/user/chat',
+    component: chat
+  },
+  {
+    path: '/login',
+    component: login
+  },
+  {
+    path: '/user/profile',
+    component: profile
+  },
+  {
+    path: '/search',
+    component: search
+  },
+  {
+    path: '/search/result',
+    component: searchresult
+  },
+  {
+    path: '/article',
+    component: article
   }
 
 ]
---
+++ src/views/user/chat.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>小志同学</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
