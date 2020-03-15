049f60befa23b538f3663fa5d2cc724e55be1265
wufan595112592 <595112592@qq.com>
2020-03-15 10:39:28 +0800

    项目脚手架初始化

---
+++ src/views/home/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+    首页
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,14 @@
+<template>
+
+<div>公共布局页面</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
